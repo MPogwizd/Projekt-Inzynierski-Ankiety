e230a6520f51ddf6e5420e2f7f4ce62aef81dacc
Michał Pogwizd <55329578+MPogwizd@users.noreply.github.com>
2021-06-14 15:52:36 +0200

    Add files via upload

---
+++ search/all_0.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['_24con_0',['$con',['../ankieta_8php.html#a0debe10448ec56a57b5509648408a549',1,'$con():&#160;ankieta.php'],['../ankieta1_8php.html#a0debe10448ec56a57b5509648408a549',1,'$con():&#160;ankieta1.php'],['../check__user_8php.html#a0debe10448ec56a57b5509648408a549',1,'$con():&#160;check_user.php'],['../dodajodp_8php.html#a0debe10448ec56a57b5509648408a549',1,'$con():&#160;dodajodp.php'],['../dodajpytania_8php.html#a0debe10448ec56a57b5509648408a549',1,'$con():&#160;dodajpytania.php'],['../show__odp__con_8php.html#a0debe10448ec56a57b5509648408a549',1,'$con():&#160;show_odp_con.php']]],
+  ['_24conection_1',['$conection',['../zaloguj_8php.html#a0f72f0fd5445ef8aaa7f5e6a48eb19d6',1,'zaloguj.php']]],
+  ['_24db_5fname_2',['$db_name',['../config_8php.html#a26dcb19f4431598ddd5f58147f131bee',1,'config.php']]],
+  ['_24db_5fpassword_3',['$db_password',['../config_8php.html#a2962c2bda65ca19dd4dd12d7160f1bf8',1,'config.php']]],
+  ['_24db_5fuser_4',['$db_user',['../config_8php.html#a6a2ff59abacecba75a151826de414a27',1,'config.php']]],
+  ['_24host_5',['$host',['../config_8php.html#a711797613cb863ca0756df789c396bf2',1,'config.php']]],
+  ['_24kod_6',['$kod',['../ankieta1_8php.html#a4232ae9ee1c035141025c149820aa89d',1,'ankieta1.php']]],
+  ['_24login_7',['$login',['../zaloguj_8php.html#afc31993e855f9631572adfedcfe6f34b',1,'zaloguj.php']]],
+  ['_24password_8',['$password',['../zaloguj_8php.html#a607686ef9f99ea7c42f4f3dd3dbb2b0d',1,'zaloguj.php']]],
+  ['_24result_9',['$result',['../ankieta_8php.html#a112ef069ddc0454086e3d1e6d8d55d07',1,'$result():&#160;ankieta.php'],['../ankieta1_8php.html#a112ef069ddc0454086e3d1e6d8d55d07',1,'$result():&#160;ankieta1.php'],['../check__user_8php.html#a112ef069ddc0454086e3d1e6d8d55d07',1,'$result():&#160;check_user.php'],['../dodajodp_8php.html#a112ef069ddc0454086e3d1e6d8d55d07',1,'$result():&#160;dodajodp.php'],['../dodajpytania_8php.html#a112ef069ddc0454086e3d1e6d8d55d07',1,'$result():&#160;dodajpytania.php'],['../show__odp__con_8php.html#a112ef069ddc0454086e3d1e6d8d55d07',1,'$result():&#160;show_odp_con.php']]],
+  ['_24z_10',['$z',['../ankieta_8php.html#a7359f3ea32073ea20d3176243cb5f86d',1,'$z():&#160;ankieta.php'],['../ankieta1_8php.html#a7359f3ea32073ea20d3176243cb5f86d',1,'$z():&#160;ankieta1.php'],['../check__user_8php.html#a7359f3ea32073ea20d3176243cb5f86d',1,'$z():&#160;check_user.php'],['../dodajodp_8php.html#a7359f3ea32073ea20d3176243cb5f86d',1,'$z():&#160;dodajodp.php'],['../dodajpytania_8php.html#a7359f3ea32073ea20d3176243cb5f86d',1,'$z():&#160;dodajpytania.php'],['../show__odp__con_8php.html#a7359f3ea32073ea20d3176243cb5f86d',1,'$z():&#160;show_odp_con.php']]]
+];
---
+++ search/all_3.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['dodajankiete_2ephp_16',['dodajankiete.php',['../dodajankiete_8php.html',1,'']]],
+  ['dodajodp_2ephp_17',['dodajodp.php',['../dodajodp_8php.html',1,'']]],
+  ['dodajpytania_2ephp_18',['dodajpytania.php',['../dodajpytania_8php.html',1,'']]],
+  ['dodawanie_2ephp_19',['dodawanie.php',['../dodawanie_8php.html',1,'']]]
+];
